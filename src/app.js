612482627117048eaa75f6d1ca8c43ca385b3b83
Julian Kohlman <juliankohlman@gmail.com>
2018-02-18 19:15:14 -0500

    set-up config format api request

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+const config = require('../config.js');
+
+const app = express();
