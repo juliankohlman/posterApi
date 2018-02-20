423d9658fb5bd8780fe49bbe4ee53daca501557c
Julian Kohlman <juliankohlman@gmail.com>
2018-02-19 21:18:37 -0500

    Set-up get requests for movie watch list posters

--- src/app.js
+++ src/app.js
@@ -5,3 +5,12 @@ const fetch = require('node-fetch');
 const config = require('../config.js');
 
 const app = express();
+const PORT = config.port;
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(`error starting server: ${err}`)
+  } else {
+    console.log(`app listening on port ${PORT}`);
+  }
+});
